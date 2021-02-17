1e4111281100d3cd78f70f511bf7b09583cda2b4
Matt Ives <mives@co.wood.wi.us>
2021-02-17 16:28:01 -0600

    Added projects and dependencies to solution

---
+++ App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TcmServiceExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
