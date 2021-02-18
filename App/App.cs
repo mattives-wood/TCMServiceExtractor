9e45f1d95041722f358475fa5711bf017e82fa5a
Matt Ives <mives@co.wood.wi.us>
2021-02-18 10:00:02 -0600

    Added appsettings.json and connection string. Added DI for config.

---
+++ App/App.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace App
+{
+    public class App
+    {
+        private readonly IConfiguration _config;
+        
+        public App(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public void Run()
+        {
+            //do things
+        }
+    }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace TcmServiceExtractor
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace App
 {
-    class Program
+    internal static class Program
     {
+        private static IConfiguration _Configuration;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+            serviceProvider.GetService<App>().Run();
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            _Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            serviceCollection.AddSingleton(_Configuration);
+
+            serviceCollection.AddTransient<App>();
         }
     }
 }
