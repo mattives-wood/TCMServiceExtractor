9378972d608078901c103b07a6ad7be66767d5bc
Matt Ives <mives@co.wood.wi.us>
2021-03-10 16:44:57 -0600

    Added options in appconfig for filtering and mode of reporting. Processes multiple clients by different modes.

--- App/App.cs
+++ App/App.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data.SqlTypes;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Xml.XPath;
 
 using Data;
 
 using Domain;
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.Extensions.Configuration;
 
 using PdfLib;
@@ -30,35 +36,114 @@ namespace App
 
         public void Run()
         {
-            // List<Contacts> contacts = _context.Contacts.Where(c => c.ClientId == 117315)
-            // .Include(c => c.Client)
-            // .Include(c => c.ProgramLkp)
-            // .Include(c => c.ServiceCode)
-            // .Include(c => c.LocationLkp)
-            // .Include(c => c.CreateStaffEmployee)
-            // .Include(c => c.StaffEmployee)
-            // .Include(c => c.EntryStaffEmployee)
-            // .Include(c => c.SignedByStaffEmployee)
-            // .Include(c => c.ProgressNotes)
-            // .Include(c => c.Intake)
-            // .ToList();
-            // Console.ReadKey();
+            int? ClientId = _config.GetSection("Parameters").GetValue<int?>("ClientId");
+            DateTime StartDate = _config.GetSection("Parameters").GetValue<DateTime>("StartDate");
+            DateTime EndDate = _config.GetSection("Parameters").GetValue<DateTime>("EndDate").AddDays(1).AddMilliseconds(-1);
+            string mode = _config.GetSection("Parameters").GetValue<string>("Mode");
 
-            Client client = _context.Clients
-            .Include(c => c.Contacts)
-            .ThenInclude(c => c.StaffEmployee)
-            .Include(c => c.Contacts)
-            .ThenInclude(c => c.ServiceCode)
-            .Include(c => c.Contacts.Where(co => co.ServDate >= new DateTime(2021, 1, 1)))
-            .ThenInclude(c => c.ProgressNotes)
-            .Where(c => c.ClientId == 117315).FirstOrDefault();
-            if (client == null)
+            PDFDocument.Mode Mode = mode switch
+            {
+                "S" => PDFDocument.Mode.Single,
+                "D" => PDFDocument.Mode.Daily,
+                "M" => PDFDocument.Mode.Monthly,
+                "Y" => PDFDocument.Mode.Yearly,
+                _ => throw new ArgumentException("Mode is incorrectly specified")
+            };
+
+            // Func<Client, bool> clientFunc = (Expression.Lambda<Func<Client, bool>>(body: Expression.Constant(true), parameters: Expression.Parameter(typeof(Client), "x"))).Compile();
+
+            // if (ClientId != null) 
+            // {
+            // ParameterExpression param = Expression.Parameter(typeof(Client), "x");
+            // MemberExpression member = Expression.Property(param, "ClientId");
+            // Type propertyType = ((PropertyInfo)member.Member).PropertyType;
+            // ConstantExpression constant = Expression.Constant(ClientId.Value);
+            // Expression expression = Expression.Equal(member, constant);
+            // Expression<Func<Client, bool>> funcExpression = Expression.Lambda<Func<Client, bool>>(body: expression, parameters: param);
+            // clientFunc = funcExpression.Compile();
+            // }
+
+            // Func<Contacts, bool> startDateFunc = (Expression.Lambda<Func<Contacts, bool>>(body: Expression.Constant(true), parameters: Expression.Parameter(typeof(Contacts), "x"))).Compile();
+            // Func<Contacts, bool> endDateFunc = (Expression.Lambda<Func<Contacts, bool>>(body: Expression.Constant(true), parameters: Expression.Parameter(typeof(Contacts), "x"))).Compile();
+            // Expression < Func<Contacts, bool> > funcExpression = null;
+            // if (StartDate != null)
+            // {
+            // ParameterExpression param = Expression.Parameter(typeof(Contacts), "x");
+            // MemberExpression member = Expression.Property(param, "ServDate");
+            // Type propertyType = ((PropertyInfo)member.Member).PropertyType;
+            // ConstantExpression constant = Expression.Constant(StartDate, typeof(DateTime?));
+            // Expression expression = Expression.GreaterThanOrEqual(member, constant);
+            // funcExpression = Expression.Lambda<Func<Contacts, bool>>(body: expression, parameters: param);
+            // startDateFunc = funcExpression.Compile();
+            // }
+
+            // if (EndDate != null)
+            // {
+            // ParameterExpression param = Expression.Parameter(typeof(Contacts), "x");
+            // MemberExpression member = Expression.Property(param, "ServDate");
+            // Type propertyType = ((PropertyInfo)member.Member).PropertyType;
+            // ConstantExpression constant = Expression.Constant(EndDate, typeof(DateTime?));
+            // Expression expression = Expression.LessThanOrEqual(member, constant);
+            // Expression<Func<Contacts, bool>> funcExpression = Expression.Lambda<Func<Contacts, bool>>(body: expression, parameters: param);
+            // endDateFunc = funcExpression.Compile();
+            // }
+
+            IQueryable<Client> query = _context.Clients
+                                .Include(c => c.Contacts)
+                                .ThenInclude(c => c.StaffEmployee)
+                                .Include(c => c.Contacts)
+                                .ThenInclude(c => c.ServiceCode)
+                                .Include(c => c.Contacts)
+                                .ThenInclude(c => c.ProgramLkp)
+                                .Include(c => c.Contacts)
+                                .ThenInclude(c => c.LocationLkp)
+                                .Include(c => c.Contacts)
+                                .ThenInclude(c => c.EntryStaffEmployee);
+
+            if (StartDate != null || EndDate != null)
+            {
+                if (EndDate == null)
+                {
+                    query = query.Include(
+                        c => c.Contacts.Where(co => co.ServDate >= StartDate).OrderBy(co => co.ServDate)).ThenInclude(co => co.ProgressNotes);
+                }
+
+                if (StartDate == null)
+                {
+                    query = query.Include(
+                        c => c.Contacts.Where(co => co.ServDate <= EndDate).OrderBy(co => co.ServDate)).ThenInclude(co => co.ProgressNotes);
+                }
+
+                query = query.Include(
+                        c => c.Contacts.Where(co => co.ServDate >= StartDate && co.ServDate <= EndDate).OrderBy(co => co.ServDate)).ThenInclude(co => co.ProgressNotes);
+            }
+            else
+            {
+                query = query.Include(
+                        c => c.Contacts.OrderBy(co => co.ServDate)).ThenInclude(co => co.ProgressNotes);
+            }
+
+            if (ClientId != null)
+            {
+                query = query.Where(c => c.ClientId == ClientId);
+            }
+
+            query = query.OrderBy(c => c.LastName).ThenBy(c => c.FirstName);
+
+            List<Client> clients = query.ToList();
+
+            if (clients.Count == 0)
             {
                 throw new Exception();
             }
 
-            PDFDocument pdf = new PDFDocument();
-            pdf.GeneratePdf(client);
+            Console.WriteLine($"Client count: {clients.Count()}");
+
+            foreach (Client c in clients)
+            {
+                PDFDocument pdf = new PDFDocument();
+                pdf.GeneratePdf(c, Mode);
+            }
         }
     }
 }
